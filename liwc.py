ce78a0fa42d286d83717e9c0bd2494f72d5268ba
love@liwenchao <1291532559@qq.com>
2019-01-04 11:59:04 +0800

    新功能开发完毕

---
+++ liwc.py
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stderr.h>
+
+public void main(int argcs[]){
+ 
+        
+        }
+
--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
-iadfjaosfja
+package com.liwc.demo
+
+import org.springframework.*;
+
+public class Login(String name,String passWord){
+	
+	if (name != null && !name.equals("")){
+
+	}else{
+		system.out.println("名字为注册');:wq
+	}	
+}
