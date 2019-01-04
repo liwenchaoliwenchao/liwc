fc0df636d20bc77e13657b7bd31e495694a74d62
love@liwenchao <1291532559@qq.com>
2019-01-04 12:08:54 +0800

    开发到一半

--- test.sh
+++ test.sh
@@ -7,6 +7,12 @@ public class Login(String name,String passWord){
 	if (name != null && !name.equals("")){
 
 	}else{
-		system.out.println("名字为注册');:wq
-	}	
+		system.out.println("名字为注册');
+	}
+
+	if (passWord != null && !passWord.equals("")){
+
+        }else{
+                system.out.println("名字为注册');
+        }
 }
